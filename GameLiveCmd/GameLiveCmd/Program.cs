a1e5b54f3ea37fba31777b789d61d672078de843
Sergey <rsv-astana@mail.ru>
2014-12-27 21:57:19 +0600

    First steps

--- GameLiveCmd/GameLiveCmd/Program.cs
+++ GameLiveCmd/GameLiveCmd/Program.cs
@@ -6,10 +6,69 @@ using System.Threading.Tasks;
 
 namespace GameLiveCmd
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var cglr = new ConsoleGameOfLiveRealisator();
+            cglr.WriteFirstGeneration();
+            Console.Read();
+        }
+
+        public interface IGameOfLiveRealisator
+        {
+            void WriteLiveDot();
+            void WriteDeadDot();
+        }
+
+        public class ConsoleGameOfLiveRealisator : IGameOfLiveRealisator
+        {
+            private const string LiveDotChar = "|-|";
+            private const string DeadDotChar = "| |";
+
+            public void WriteFirstGeneration()
+            {
+                var grid = new byte[20, 20];
+                grid = MakeGrid(grid);
+                for (int i = 0; i < 20; i++)
+                {
+                    for (int j = 0; j < 20; j++)
+                    {
+                        if (grid[i, j] == 1)
+                        {
+                            WriteLiveDot();
+                        }
+                        else
+                        {
+                            WriteDeadDot();
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            private byte[,] MakeGrid(byte[,] grid)
+            {
+                var rnd = new Random(DateTime.Now.Second);
+                for (int i = 0; i < 20; i++)
+                {
+                    for (int j = 0; j < 20; j++)
+                    {
+                        grid[i, j] = (byte) rnd.Next(2);
+                    }
+                }
+                return grid;
+            }
+
+            public void WriteLiveDot()
+            {
+                Console.Write(LiveDotChar);
+            }
+
+            public void WriteDeadDot()
+            {
+                Console.Write(DeadDotChar);
+            }
         }
     }
 }
